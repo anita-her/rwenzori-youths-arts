daaafc4431223f96e6d5467155f84da907f21861
anita-her <anitangabirano755@gmail.com>
2025-03-17 12:05:15 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector("form").addEventListener("submit", function (event) {
+        event.preventDefault();
+        alert("Thank you for reaching out! We will get back to you soon.");
+    });
+});
